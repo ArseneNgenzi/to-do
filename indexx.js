038e01a92db37898dfeb408c6eef08a3c5ba3a71
ArseneNgenzi <arsenengenzi@gmail.com>
2023-05-05 23:36:31 +0300

    Added Priority

--- indexx.js
+++ indexx.js
@@ -2,6 +2,8 @@ const addTodo = document.querySelector("#add-todo");
 const todoInput = document.querySelector("#input-value");
 const todoParentContainer = document.querySelector("#todo-parent-container");
 const errMsg = document.querySelector('#err-msg')
+const priority = document.querySelector('#priority')
+
 
 const showError = () => {
 		errMsg.classList.remove('hidden')
@@ -9,16 +11,29 @@ const showError = () => {
 const hideError = () => {
 	errMsg.classList.add('hidden')
 }
+// let date
 
 // Show Todos on page function
 const showTodos = () => {
 	let AllTodosss = JSON.parse(localStorage.getItem("todos")) || [];
+
+	// console.log(AllTodosss)
+	// console.log(priority.value)
+
 	let html1 = "";
-	AllTodosss.forEach((todo) => {
-		html1 += `<div class="flex items-center" >
-        <p class="to-do-item p-2 grow" id=""> ${todo}</p>
-        <span class="material-symbols-outlined text-red-600 cursor-pointer" onClick="deleteItem('${todo}')">delete</span>
-    </div>`;
+	AllTodosss.forEach((todo, index) => {
+		html1 += `<div class="p-2 border-b border-black " >
+		<div class="flex items-center"> 
+			<p class="to-do-item  grow font-bold" id=""> ${todo.item}</p>
+			<span class="material-symbols-outlined text-red-600 cursor-pointer" onClick="deleteItem('${todo.id}')">delete</span>
+		</div>
+		<div class="text-sm flex justify-between">
+		<p> Priority <span class="text-blue-600"> ${todo.priority} </span></p>
+		<p>${todo.date}</p>
+		</div>
+		
+	</div>
+		`;
 	});
 	todoParentContainer.innerHTML = html1;
 };
@@ -30,11 +45,19 @@ showTodos()
 // Add todo in local storage then display it using showtodo() function
 addTodo.addEventListener("click", () => {
 	if (todoInput.value) {
+		
+		let aTodoObject = {
+			id: Date.now(),
+			item: todoInput.value,
+			date: new Date().toLocaleString(),
+			priority: priority.value
+		}
 		let AllTodos = JSON.parse(localStorage.getItem("todos")) || [];
-		AllTodos.push(todoInput.value);
+		AllTodos.push(aTodoObject);
 		localStorage.setItem("todos", JSON.stringify(AllTodos));
 		todoInput.value = "";
 		showTodos();
+		// console.log(aTodoObject)
 	} 
 	else {
 		showError()
@@ -44,8 +67,14 @@ addTodo.addEventListener("click", () => {
 
 todoInput.addEventListener("keydown", (e) => {
 	if (e.keyCode === 13 && todoInput.value) {
+		let aTodoObject = {
+			id: Date.now(),
+			item: todoInput.value,
+			date: new Date().toLocaleString(),
+			priority: priority.value
+		}
 		let AllTodos = JSON.parse(localStorage.getItem("todos")) || [];
-		AllTodos.push(todoInput.value);
+		AllTodos.push(aTodoObject);
 		localStorage.setItem("todos", JSON.stringify(AllTodos));
 		todoInput.value = "";
 		showTodos();
@@ -57,11 +86,14 @@ todoInput.addEventListener("keydown", (e) => {
 	
 });
 
-const deleteItem = (word) => {
+const deleteItem = (id) => {
 	let AllTodos = JSON.parse(localStorage.getItem("todos")) || [];
-	let newTodos = AllTodos.filter((item) => item !== word);
+	// console.log('AllTodos: ', AllTodos)
+	let newTodos = AllTodos.filter((item) => item.id != id);
+	// console.log('Filtered: ', newTodos)
 	localStorage.setItem("todos", JSON.stringify(newTodos));
 	showTodos();
+	// console.log(id)
 };
 
 
